a46c2b898082cf3056d26fa0a59615fab4ef3260
cornelius-weigert <cornelius.weigert@uni-bayreuth.de>
2025-11-03 17:50:42 +0100

    played around with streamlit, not yet able to print pictures created by pm4py

--- practice/pm4py-test.py
+++
@@ -1,9 +0,0 @@
-import pm4py
-log = pm4py.read_xes('C:/Users/Corne/Dokumente/studium/Semester 5/SWPS/practice/running-example.xes')
-
-# Discover the directly follows graph
-directly_follows_graph, start_activities, end_activities = pm4py.discover_dfg(log)
-bpmn= pm4py.discover_bpmn_inductive(log)
-# Visualize the DFG
-pm4py.view_dfg(directly_follows_graph, start_activities, end_activities,format='svg',graph_title="Directly Follows Graph")
-pm4py.view_bpmn(bpmn,format='svg',graph_title="Hello World BPMN")
---
+++ practice/pm4py_test.py
@@ -0,0 +1,24 @@
+import pm4py
+
+def main():
+    log = pm4py.read_xes('C:/Users/Corne/Dokumente/studium/Semester 5/SWPS/practice/running-example.xes')
+
+    # Discover the directly follows graph
+    directly_follows_graph, start_activities, end_activities = pm4py.discover_dfg(log)
+    bpmn= pm4py.discover_bpmn_inductive(log)
+    # Visualize the DFG
+    pm4py.view_dfg(directly_follows_graph, start_activities, end_activities,format='svg',graph_title="Directly Follows Graph")
+    pm4py.view_bpmn(bpmn,format='svg',graph_title="Hello World BPMN")
+
+def get_dfg_image(log):
+    directly_follows_graph, start_activities, end_activities = pm4py.discover_dfg(log)
+    dfg_image = pm4py.view_dfg(directly_follows_graph, start_activities, end_activities,format='svg',graph_title="Directly Follows Graph")
+    return dfg_image
+
+def get_bpmn_image(log):
+    bpmn= pm4py.discover_bpmn_inductive(log)
+    bpmn_image = pm4py.view_bpmn(bpmn,format='svg',graph_title="Hello World BPMN")
+    return bpmn_image
+
+if __name__ == "__main__":
+    main()
