af06cd8ba649e60a22c39640e7472f85dad8fcb6
Janne B. <janne.berger@uni-bayreuth.de>
2025-12-14 15:51:37 +0100

    session state removed

--- pages/1__Tabelle.py
+++ pages/1__Tabelle.py
@@ -15,20 +15,6 @@ from Datenanalyse_Outlier.display_analysis.main import show_all_analysis
 from Datenanalyse_Outlier.eventlog_to_dataframe import eventlog_to_df
 from Datenanalyse_Outlier.map_columns import map_column
 
-
-# --- SESSION STATE INITIALISIEREN ---
-if "uploaded_logs" not in st.session_state or st.session_state["uploaded_logs"] is None:
-    st.session_state["uploaded_logs"] = []
-
-if not isinstance(st.session_state["uploaded_logs"], list):
-    st.session_state["uploaded_logs"] = list(st.session_state["uploaded_logs"])
-
-st.session_state.setdefault("latest_upload", None)
-st.session_state.setdefault("file_path", None)
-st.session_state.setdefault("file_type", None)
-st.session_state.setdefault("file_name", None)
-st.session_state.setdefault("df", None)
-st.session_state.setdefault("log", None)
 # Sonstige Session States für Ausreißer
 st.session_state.setdefault("outlier_total", 0)
 st.session_state.setdefault("outlier_checked", 0)  
